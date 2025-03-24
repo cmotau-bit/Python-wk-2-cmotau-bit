00726a7783b6fb88c68fee65ec8ff7a9d1fe7836
cmotau-bit <cmotau@gmail.com>
2025-03-24 23:47:51 +0200

    Create Answers.py
    Week 2 submission

---
+++ Answers.py
@@ -0,0 +1,30 @@
+#Question 1
+my_list = []
+
+#Question 2
+# Appending elements
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+#Question 3
+# Display the updated list
+print(my_list)
+
+#Question 4
+my_list = [10, 20, 30, 40]
+my_list.insert(1, 15)
+print(my_list)
+
+#Question 5
+# Another list to extend with
+another_list = [50, 60, 70]
+
+#Question 6
+# Extend the original list with another list
+my_list.extend(another_list)
+
+#Question 7
+#print updated list
+print(my_list)
